62da038cd9b3c69fd9a5d6848f7281afee328eb6
Ralf S. Engelschall <rse@engelschall.com>
2021-10-09 13:13:44 +0200

    upgrade to Vue 3

---
+++ index.vue
@@ -0,0 +1,24 @@
+
+<template>
+    <hello
+        v-bind:background="config.hello.background"
+        v-bind:foreground="config.hello.foreground"
+        v-bind:text="config.hello.text"
+    ></hello>
+</template>
+
+<style lang="less" scoped>
+</style>
+
+<script>
+module.exports = {
+    name: "index",
+    data: () => ({
+        config: huds.config()
+    }),
+    components: {
+        "hello": Vue.loadComponent("hello.vue")
+    }
+}
+</script>
+
